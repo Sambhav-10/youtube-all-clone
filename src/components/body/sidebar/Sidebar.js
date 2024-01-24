74edce2c0a08ce724461a2fd394e6935ba1d7960
Sambhav jain <fingma624@gmail.com>
2024-01-25 00:08:21 +0530

    style fix : sidebar

--- src/components/body/sidebar/Sidebar.js
+++ src/components/body/sidebar/Sidebar.js
@@ -6,8 +6,8 @@ const Sidebar = () => {
     const isManuOpen = useSelector((store) => store.nav.isManuOpen)
 
   return !isManuOpen ? null : (
-    <div className='w-[200px] bg-green-300 h-[90vh] '>
-        <section className='border '>
+    <div className='w-[200px]  h-[90vh] fixed bg-slate-400 '>
+        <section className='p-1 '>
             <div className='flex items-center'>
                 <img className='w-1/3' src="https://endlessicons.com/wp-content/uploads/2012/11/home-icon-614x460.png" alt="" />
                 <Link to={"/"}><p>Home</p></Link>
@@ -16,8 +16,9 @@ const Sidebar = () => {
                 <img className='w-1/3' src="https://cdn.pixabay.com/photo/2023/03/30/17/30/ai-generated-7888387_640.png" alt="" />
                 <p>Shorts</p>
             </div>
-            <div className='flex items-center'>
-                <img className=' w-1/4' src="https://cdn.icon-icons.com/icons2/3237/PNG/512/menu_youtube_social_media_subs_subscription_icon_197393.png" alt="" />
+           
+            <div className='flex  items-center w-full'>
+                <img className='ml-2 w-1/5' src="https://cdn.icon-icons.com/icons2/3237/PNG/512/menu_youtube_social_media_subs_subscription_icon_197393.png" alt="" />
                 <p>Subscriptions</p>
             </div>
         </section>
