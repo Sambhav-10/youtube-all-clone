e8643b96d4f9be61bb7a4b846656504ed376970c
Sambhav jain <fingma624@gmail.com>
2024-01-25 00:10:30 +0530

    Search fix (scroll fix):Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,26 +3,51 @@ import "./nav.css";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../../utils/navSlice";
 import { YOUR_SEARCH_API } from "../../utils/contains";
-import { wait } from "@testing-library/user-event/dist/utils";
-import { json } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggention, setSuggention] = useState([]);
+  const [showsuggention, setShowSuggention] = useState(false);
 
-    const [searchQuery,setSearchQuery] = useState("")
-useEffect(()=>{
-getSearchQuery()
-},[searchQuery])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getSearchQuery();
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
 
-const getSearchQuery = async ()=>{
-const data = await fetch(YOUR_SEARCH_API + searchQuery);
-const res = await data.json()
-console.log(res);
-}
+ 
+
+  }, [searchQuery]);
+  
+  useEffect(() => {
+
+    window.addEventListener("scroll",close)
+      
+        return () => window.removeEventListener("scroll", close);
+    
+  }, [searchQuery]);
+
+  function close(){
+   setShowSuggention(false) 
+  }
+
+
+  const getSearchQuery = async () => {
+    const data = await fetch(YOUR_SEARCH_API + searchQuery);
+    const res = await data.json();
+    console.log(res);
+    setSuggention(res[1]);
+  };
 
   const dispatch = useDispatch();
 
   const toggleMenuhandler = () => {
     dispatch(toggleMenu());
+    console.log("no");
   };
   return (
     <nav className="flex h-20 w-full justify-between shadow-lg p-5">
@@ -33,21 +58,50 @@ console.log(res);
           alt=""
           onClick={() => toggleMenuhandler()}
         />
+     
         <img
           className="w-[150px] h-[60px]"
           src="https://t3.ftcdn.net/jpg/03/00/38/90/360_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg"
           alt=""
-        />
+          />
+          
       </div>
-      <div className="flex items-center ">
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e)=> setSearchQuery(e.target.value)}
-          className="w-80 h-10 border border-gray-900 rounded-l-full"
-        />
-        <button className="bg-gray-400 h-10 rounded-e-full p-2">search</button>
+      <div className="r ">
+        <div>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value)
+            setShowSuggention(true)
+            }
+            
+            }
+            className="w-80 h-10 border border-gray-900 rounded-l-full px-4"
+            onFocus={() => setShowSuggention(true)}
+            onBlur={() => setShowSuggention(false)}
+            
+          />
+          <button className="bg-gray-400 h-10 rounded-e-full p-2">
+            search
+          </button>
+        </div>
+
+        {showsuggention ? (
+          <div className="fixed z-20 bg-white w-80 p-2 shadow-lg rounded-lg">
+            <ul className="">
+              {suggention.map((e, i) => {
+                return (
+                  <li className="cursor-pointer  p-2 shadow-lg" key={i} >
+                    {e}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : null}
       </div>
+
       <div className="flex w-1/6 align-middle items-center ">
         <img
           className="w-[40px] h-[40px] "
